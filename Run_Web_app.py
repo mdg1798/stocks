ef184767c5392dcff5adb796ca4de480369da1c8
mdg1798 <55677016+mdg1798@users.noreply.github.com>
2020-08-22 01:44:18 -0700

    Update Run_Web_app.py

--- Run_Web_app.py
+++ Run_Web_app.py
@@ -230,11 +230,11 @@ def update_graph12(n_clicks, value, start_date,end_date,my_ticker_symbol):
 
 
 # In[17]:
-
+port = int(os.environ.get(“PORT”, 5000))
 
 if __name__ == "__main__":
     #app.run_server(port=8017)
-    app.server.run()
+    app.server.run(port=port)
 
 
 # In[ ]:
